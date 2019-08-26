438fe735bf426ddbd7bdcdf705572eccdbdad50b
Bible <ptang8@bus.illinois.edu>
2019-08-26 19:46:48 +0900

    feat: add introduction parts to keynote

---
+++ code-snippet/lines-of-html.sh
@@ -0,0 +1,7 @@
+❯ wc -l item-transactions*.html
+     152 item-transactions-yamato-address-from.html
+     147 item-transactions-yamato-address-to.html
+    1805 item-transactions.jp.html
+     314 item-transactions.jp.official-store.html
+    1159 item-transactions.jp.yamato-kazai.html
+    3577 total
---
+++ code-snippet/lines-of-js.sh
@@ -0,0 +1,5 @@
+❯ wc -l item_transactions.*.js
+     817 item_transactions.common.js
+    2851 item_transactions.jp.js
+    1589 item_transactions.jp.yamato_kazai.js
+    5257 total
